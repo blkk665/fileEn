077d10459fbd8a6ba46c924f8ce08ac48ab5ab21
blkk665 <qianlan2000@qq.com>
2022-10-17 10:58:10 +0800

    更换返回文件下载测试接口存放路径

--- src/main/java/com/blkk665/fileen/controller/HttpFileController.java
+++ src/main/java/com/blkk665/fileen/controller/HttpFileController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
 
 /**
  * @Description
@@ -47,6 +47,58 @@ public class HttpFileController {
                        @RequestParam(value = "fileName") String fileName) throws Exception {
 //        decryptFile(filePath, fileName);
 
+    }
+
+
+
+    @GetMapping("/download2")
+    public String downloadFile2( HttpServletResponse response) throws IOException {
+        // 获取指定目录下的文件
+        String fileName = "/Users/pluttt/Downloads/2.mp4";
+        File file = new File(fileName);
+        // 如果文件名存在，则进行下载
+        if (file.exists()) {
+            // 配置文件下载
+            response.setHeader("content-type", "application/octet-stream");
+            response.setContentType("application/octet-stream");
+            // 下载文件能正常显示中文
+            response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+
+            // 实现文件下载
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null;
+            BufferedInputStream bis = null;
+            try {
+                fis = new FileInputStream(file);
+                bis = new BufferedInputStream(fis);
+                OutputStream os = response.getOutputStream();
+                int i = bis.read(buffer);
+                while (i != -1) {
+                    os.write(buffer, 0, i);
+                    i = bis.read(buffer);
+                }
+                System.out.println("Download the song successfully!");
+            }
+            catch (Exception e) {
+                System.out.println("Download the song failed!");
+            } finally {
+                if (bis != null) {
+                    try {
+                        bis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (fis != null) {
+                    try {
+                        fis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return null;
     }
 
 
